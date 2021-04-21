b2f7ebdf1899929db5dfe988da09e3457ca1604d
Miles Crighton <miles.crighton@gmail.com>
2021-04-21 10:09:33 +0100

    Add organize imports command to tsserver lsp config

--- lua/lsp/js-ts-ls.lua
+++ lua/lsp/js-ts-ls.lua
@@ -7,11 +7,24 @@
 -- require'completion'.on_attach(client)
 -- require'illuminate'.on_attach(client)
 -- end
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = {vim.api.nvim_buf_get_name(0)},
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
+function tsserver_on_attach(client, bufnr)
+    require"lsp".common_on_attach(client, bufnr)
+    client.resolved_capabilities.document_formatting = false
+end
 
 require'lspconfig'.tsserver.setup {
     cmd = {DATA_PATH .. "/lspinstall/typescript/node_modules/.bin/typescript-language-server", "--stdio"},
     filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-    on_attach = require'lsp'.tsserver_on_attach,
+    on_attach = tsserver_on_attach,
     -- This makes sure tsserver is not used for formatting (I prefer prettier)
     -- on_attach = require'lsp'.common_on_attach,
     root_dir = require('lspconfig/util').root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
@@ -24,5 +37,11 @@ require'lspconfig'.tsserver.setup {
             update_in_insert = true
 
         })
-    }
-}
+    },
+    commands = {
+		OrganizeImports = {
+		  organize_imports,
+		  description = "Organize Imports"
+		}
+	  }
+	}
