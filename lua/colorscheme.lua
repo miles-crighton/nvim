3bc90b6a4a30fefe404619cd6560dae004e56bfc
Miles Crighton <miles.crighton@gmail.com>
2021-04-21 10:27:58 +0100

    Reduce background colors for git diff add/delete

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,3 +1,7 @@
 vim.cmd('let g:nvcode_termcolors=256')
 
 vim.cmd('colorscheme ' .. O.colorscheme)
+
+-- Change diff colors for reduced contrast
+vim.cmd('hi DiffAdd gui=NONE guifg=NONE guibg=#3d4543')
+vim.cmd('hi DiffDelete gui=NONE guifg=NONE guibg=#3e303a')
