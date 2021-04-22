a8b7dfdbeb7d098deb3920df01b2b19191dd3436
Miles Crighton <miles.crighton@gmail.com>
2021-04-22 21:35:20 +0100

    Open correct settings file on dashboard

--- lua/dashboard/init.lua
+++ lua/dashboard/init.lua
@@ -17,5 +17,5 @@ vim.g.dashboard_custom_section = {
     b = {description = {'  Recently Usd Files'}, command = 'Telescope oldfiles'},
     c = {description = {'  Load Last Session  '}, command = 'SessionLoad'},
     d = {description = {'  Find Word          '}, command = 'Telescope live_grep'},
-    e = {description = {'  Settings           '}, command = ':e ~/.config/nvim/lv-settings.lua'}
+    e = {description = {'  Settings           '}, command = ':e ~/.config/nvim/settings.lua'}
 }
