6f152e73b344cd374c7f5064bfac24b48177db3b
Miles Crighton <miles.crighton@gmail.com>
2021-05-10 17:09:10 +0100

    Darken current line highlight

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -5,3 +5,6 @@ vim.cmd('colorscheme ' .. O.colorscheme)
 -- Change diff colors for reduced contrast
 vim.cmd('hi DiffAdd gui=NONE guifg=NONE guibg=#3d4543')
 vim.cmd('hi DiffDelete gui=NONE guifg=NONE guibg=#3e303a')
+
+-- Darken current line highlight
+vim.cmd('hi CursorLine guibg=#1f2335')
