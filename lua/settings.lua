be050641d642e63f5e1243c1ca91717acbf80770
Miles Crighton <miles.crighton@gmail.com>
2021-05-11 20:45:37 +0100

    Minor tweaks to settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,7 +1,7 @@
 vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object"
 vim.cmd('set shortmess+=c') -- Don't pass messages to |ins-completion-menu|.
 vim.cmd('set inccommand=split') -- Make substitution work in realtime
-vim.cmd('set autochdir') -- change the dir to current file
+-- vim.cmd('set autochdir') -- change the dir to current file
 vim.o.hidden = O.hidden_files -- Required to keep multiple buffers open multiple buffers
 vim.o.title = true
 TERMINAL = vim.fn.expand('$TERMINAL')
@@ -20,16 +20,17 @@ vim.o.termguicolors = true -- set term giu colors most terminals support this
 vim.o.splitright = true -- Vertical splits will automatically be to the right
 vim.o.t_Co = "256" -- Support 256 colors
 vim.o.conceallevel = 0 -- So that I can see `` in markdown files
-vim.cmd('set ts=4') -- Insert 2 spaces for a tab
-vim.cmd('set sw=4') -- Change the number of space characters inserted for indentation
+vim.cmd('set ts=2') -- Insert 2 spaces for a tab
+vim.cmd('set sw=2') -- Change the number of space characters inserted for indentation
+vim.cmd('set expandtab') -- uses spaces instead of tabs
 vim.bo.expandtab = true -- Converts tabs to spaces
 vim.bo.smartindent = true -- Makes indenting smart
-vim.wo.number = O.number -- set numbered lines
+vim.wo.number = O.number -- set numbered line2
 vim.wo.relativenumber = O.relative_number -- set relative number
 vim.wo.cursorline = true -- Enable highlighting of the current line
 vim.o.showtabline = 2 -- Always show tabs
 vim.o.scrolloff = 8 -- Lines before scrolling
-vim.o.foldcolumn = "3" -- Left indent
+vim.o.foldcolumn = "2" -- Left indent
 vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore
 vim.o.backup = false -- This is recommended by coc
 vim.o.writebackup = false -- This is recommended by coc
@@ -43,5 +44,8 @@ vim.o.clipboard = "unnamedplus" -- Copy paste between vim and everything else
 -- vim.o.guifont = "SauceCodePro Nerd Font:h17"
 -- vim.o.guifont = "FiraCode\\ Nerd\\ Font:h17"
 vim.g.tokyonight_transparent = true
+
+-- Blamer Plugin
 vim.g.blamer_enabled = 1
 vim.g.blamer_relative_time = 1
+vim.g.blamer_show_in_visual_modes = 0
