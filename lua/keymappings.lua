97181d869472a8a35e7335ecb81d87b1f25487ae
Miles Crighton <miles.crighton@gmail.com>
2021-05-12 09:47:14 +0100

    Adds LSP hover cmd to K

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -7,6 +7,8 @@ vim.api.nvim_set_keymap('n', '<Leader>ss', ':luafile $MYVIMRC<CR>', {noremap = t
 -- no hl
 vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', {noremap = true, silent = true})
 
+vim.api.nvim_set_keymap('n', 'K', ':lua require(\'lspsaga.hover\').render_hover_doc()<CR>', {noremap = true, silent = true})
+
 -- Add a line above/below
 -- vim.api.nvim_set_keymap('n', '<ENTER>', '<C-u>call append(line("."),   repeat([""], v:count1))<CR>', {noremap = true, silent = true})
 -- vim.api.nvim_set_keymap('n', '<S-Enter>', '<C-u>call append(line(".")-1,   repeat([""], v:count1))<CR>', {noremap = true, silent = true})
