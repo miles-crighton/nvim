841ff0890b7cf579001231d1a11516aeecdeaaee
Miles Crighton <miles.crighton@gmail.com>
2021-05-16 13:25:08 +0100

    Use nvim-bufferline instead of barbar

--- lua/barbar/init.lua
+++ lua/barbar/init.lua
@@ -1,3 +1,14 @@
-vim.api.nvim_set_keymap('n', '<TAB>', ':BufferNext<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferPrevious<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<TAB>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-x>', ':BufferClose<CR>', { noremap = true, silent = true })
+
+require'bufferline'.setup{
+  options = {
+    diagnostics = "nvim_lsp",
+    diagnostics_indicator = function(count, level, diagnostics_dict)
+      local icon = level:match("error") and "" or ""
+      return " " .. count .. icon
+    end,
+    offsets = {{filetype = "NvimTree", text = "File Explorer", highlight = "Directory", text_align = "center"}}
+  }
+}
