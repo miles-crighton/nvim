b33ad1d747f31cdbacb0cc59dde2aa3b7c9e9bc6
Miles Crighton <miles.crighton@gmail.com>
2021-06-07 11:36:12 +0100

    Reintroduce autocommands import

--- lua/barbar/init.lua
+++ lua/barbar/init.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_set_keymap('n', '<TAB>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<S-TAB>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-x>', ':BufferClose<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-x>', ':BufferLineClose<CR>', { noremap = true, silent = true })
 
 require'bufferline'.setup{
   options = {
