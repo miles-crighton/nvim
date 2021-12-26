79dcfe0f1924d4bb437b91018f7b1c0328cd3766
Miles Crighton <miles.crighton@gmail.com>
2021-12-26 20:28:21 +0000

    Minimise link script

--- link.sh
+++ link.sh
@@ -1,16 +1,5 @@
 #!/usr/bin/env fish
 
-# Variables for source and dest paths
-set --local home ~
-set --local src (pwd)
-
-# For neovim
-mkdir -p $home/.config/nvim
-ln -s $src/* $home/.config/nvim
-
-# For vim - requires symlinking site and nvim files to ~/.vim
-mkdir -p $home/.local/share/nvim/site
-mkdir -p $home/.vim
-ln -s $home/.local/share/nvim/site $home/.vim
-ln -s $src/* $home/.vim
-ln -s $src/init.vim $home/.vimrc
+# Use stow from main dotfiles in future
+mkdir -p ~/.config/nvim
+ln -s (pwd)/* ~/.config/nvim
