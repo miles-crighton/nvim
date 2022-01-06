ae3099267c7e905cf9932a6295f9fe8b46d029d4
Miles Crighton <miles.crighton@gmail.com>
2022-01-06 20:06:09 +0000

    Create custom eslint null-ls source

---
+++ lua/lsp/null-ls/init.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+M.setup = function()
+	local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+	if not null_ls_status_ok then
+		return
+	end
+
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+	local formatting = null_ls.builtins.formatting
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+	local diagnostics = null_ls.builtins.diagnostics
+
+	null_ls.setup({
+		debounce = 150,
+		autostart = true,
+		debug = true,
+		sources = {
+			require("lsp.null-ls.eslint-source").setup(),
+			formatting.prettierd.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+			formatting.black.with({ extra_args = { "--fast" } }),
+			formatting.stylua,
+			diagnostics.shellcheck,
+		},
+		on_attach = function(client)
+			if client.resolved_capabilities.document_formatting then
+				vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+			end
+		end,
+	})
+end
+
+return M
