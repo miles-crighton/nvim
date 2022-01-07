4a64b1e826faf116bd2de6d4b04666c614265357
Miles Crighton <miles.crighton@gmail.com>
2022-01-07 11:04:45 +0000

    Change default term to bash - massive perf improvements

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,34 +1,35 @@
 --- Set cmds ---
-vim.cmd('set iskeyword+=-') -- treat dash separated words as a word text object"
-vim.cmd('set shortmess+=c') -- Don't pass messages to |ins-completion-menu|.
-vim.cmd('set inccommand=split') -- Make substitution work in realtime
-vim.cmd('set autoread') -- autoread changes to buffered files on file system
+vim.cmd("set iskeyword+=-") -- treat dash separated words as a word text object"
+vim.cmd("set shortmess+=c") -- Don't pass messages to |ins-completion-menu|.
+vim.cmd("set inccommand=split") -- Make substitution work in realtime
+vim.cmd("set autoread") -- autoread changes to buffered files on file system
 
 --- General Configs ---
 vim.o.fileencoding = "utf-8" -- The encoding written to ile
-vim.cmd('set colorcolumn=99999') -- fix indentline for now
+vim.cmd("set colorcolumn=99999") -- fix indentline for now
 vim.o.mouse = "a" -- Enable your mouse
 vim.o.conceallevel = 0 -- So that I can see `` in markdown files
 vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore
 vim.o.backup = false -- This is recommended by coc
 vim.o.writebackup = false -- This is recommended by coc
-vim.o.completeopt = 'menu,menuone,noselect'
+vim.o.completeopt = "menu,menuone,noselect"
+vim.o.shell = "/bin/bash" -- Fish shell causes large lag across nvim
 
 --- Line Wrapping ---
 vim.wo.wrap = false -- Display long lines as just one line
-vim.cmd('set whichwrap+=<,>,[,]') -- move to next line with theses keys
+vim.cmd("set whichwrap+=<,>,[,]") -- move to next line with theses keys
 vim.o.hidden = true -- Required to keep multiple buffers open multiple buffers
 
 --- Terminal Title ---
 vim.o.title = true
-TERMINAL = vim.fn.expand('$TERMINAL')
+TERMINAL = vim.fn.expand("$TERMINAL")
 vim.cmd('let &titleold="' .. TERMINAL .. '"')
 vim.o.titlestring = "%<%F%= - nvim"
 
 --- Tab/Space inserts ---
-vim.cmd('set ts=2') -- Insert 2 spaces for a tab
-vim.cmd('set sw=2') -- Change the number of space characters inserted for indentation
-vim.cmd('set expandtab') -- uses spaces instead of tabs
+vim.cmd("set ts=2") -- Insert 2 spaces for a tab
+vim.cmd("set sw=2") -- Change the number of space characters inserted for indentation
+vim.cmd("set expandtab") -- uses spaces instead of tabs
 vim.bo.smartindent = true -- Makes indenting smart
 vim.bo.expandtab = true -- Converts tabs to spaces
 
@@ -55,7 +56,7 @@ vim.o.ttimeoutlen = 0
 vim.g.tokyonight_transparent = true
 vim.o.termguicolors = true -- set term giu colors most terminals support this
 vim.wo.cursorline = true -- Enable highlighting of the current line
-vim.cmd('syntax on')
+vim.cmd("syntax on")
 
 --- Line numbers
 vim.wo.number = true -- set numbered line2
