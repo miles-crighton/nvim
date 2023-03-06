f428eff64d6e0d5ef7e50e8bd88123e9e8bb9b19
Miles Crighton <miles.crighton@gmail.com>
2023-03-06 09:36:51 +0000

    Conditionally load eslint

--- lua/lsp/null-ls/init.lua
+++ lua/lsp/null-ls/init.lua
@@ -1,5 +1,21 @@
 local M = {}
 
+local function eslint_config_exists()
+	local eslintrc = vim.fn.glob(".eslintrc*", 0, 1)
+
+	if not vim.tbl_isempty(eslintrc) then
+		return true
+	end
+
+	if vim.fn.filereadable("package.json") then
+		if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+			return true
+		end
+	end
+
+	return false
+end
+
 M.setup = function()
 	local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 	if not null_ls_status_ok then
@@ -15,13 +31,17 @@ M.setup = function()
 		debug = true,
 		diagnostics_format = "#{m} [#{c}]",
 		sources = {
-			diagnostics.eslint_d,
+			diagnostics.eslint_d.with({
+				condition = function(utils)
+					return eslint_config_exists()
+				end,
+			}),
 			diagnostics.eslint.with({
 				filetypes = { "svelte" },
 				-- Load the local eslint config
 				command = "./node_modules/.bin/eslint",
 			}),
-			formatting.prettier_d_slim.with({
+			formatting.prettierd.with({
 				extra_filetypes = { "svelte" },
 			}),
 			-- formatting.prettier,
