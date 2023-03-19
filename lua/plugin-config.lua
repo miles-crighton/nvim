ed716b4b545ce3469d8bddafe0b24b0daac22e0e
Miles Crighton <miles.crighton@gmail.com>
2023-03-19 11:59:30 +0000

    Lazy load gitsigns

--- lua/plugin-config.lua
+++ lua/plugin-config.lua
@@ -63,8 +63,23 @@ return {
 	-- Git
 	{
 		"lewis6991/gitsigns.nvim",
-		event = "BufRead",
+		ft = "gitcommit",
 		init = function()
+			-- load gitsigns only when a git file is opened -- from https://github.com/NvChad/NvChad/blob/v2.0/lua/plugins/init.lua#L95
+			vim.api.nvim_create_autocmd({ "BufRead" }, {
+				group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+				callback = function()
+					vim.fn.system("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse")
+					if vim.v.shell_error == 0 then
+						vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
+						vim.schedule(function()
+							require("lazy").load({ plugins = { "gitsigns.nvim" } })
+						end)
+					end
+				end,
+			})
+		end,
+		config = function()
 			require("plugins.gitsigns").setup()
 		end,
 	},
