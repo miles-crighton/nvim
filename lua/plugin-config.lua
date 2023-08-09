f975f98fd402344d6776543fc193831a7ee00768
Miles Crighton <miles.crighton@gmail.com>
2023-08-09 16:54:59 +0100

    Ignore null-ls in fidget config

--- lua/plugin-config.lua
+++ lua/plugin-config.lua
@@ -39,14 +39,14 @@ return {
 		"norcalli/nvim-colorizer.lua",
 		config = function()
 			require("colorizer").setup(nil, {
-				RGB = true,      -- #RGB hex codes
-				RRGGBB = true,   -- #RRGGBB hex codes
-				names = false,   -- "Name" codes like Blue
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				names = false, -- "Name" codes like Blue
 				RRGGBBAA = true, -- #RRGGBBAA hex codes
-				rgb_fn = true,   -- CSS rgb() and rgba() functions
-				hsl_fn = true,   -- CSS hsl() and hsla() functions
-				css = false,     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-				css_fn = false,  -- Enable all CSS *functions*: rgb_fn, hsl_fn
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 				-- Available modes: foreground, background
 				mode = "background", -- Set the display mode.
 			})
@@ -110,7 +110,13 @@ return {
 		"j-hui/fidget.nvim",
 		tag = "legacy",
 		config = function()
-			require("fidget").setup({})
+			require("fidget").setup({
+				sources = {
+					["null-ls"] = {
+						ignore = true,
+					},
+				},
+			})
 		end,
 	},
 	{
