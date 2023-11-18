84c30166086149325e1694225f051a0c7ba3f48a
Miles Crighton <miles.crighton@gmail.com>
2023-11-18 12:44:40 +0000

    Re-enable gitsigns

--- lua/plugin-config.lua
+++ lua/plugin-config.lua
@@ -45,30 +45,30 @@ return {
 			require("nvim-surround").setup()
 		end,
 	},
-	-- -- Git
-	-- -- Causing slow down on scroll
-	-- {
-	--   "lewis6991/gitsigns.nvim",
-	--   ft = "gitcommit",
-	--   init = function()
-	--     -- load gitsigns only when a git file is opened -- from https://github.com/NvChad/NvChad/blob/v2.0/lua/plugins/init.lua#L95
-	--     vim.api.nvim_create_autocmd({ "BufRead" }, {
-	--       group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-	--       callback = function()
-	--         vim.fn.system("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse")
-	--         if vim.v.shell_error == 0 then
-	--           vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
-	--           vim.schedule(function()
-	--             require("lazy").load({ plugins = { "gitsigns.nvim" } })
-	--           end)
-	--         end
-	--       end,
-	--     })
-	--   end,
-	--   config = function()
-	--     require("plugins.gitsigns").setup()
-	--   end,
-	-- },
+	-- Git
+	-- Causing slow down on scroll
+	{
+		"lewis6991/gitsigns.nvim",
+		ft = "gitcommit",
+		init = function()
+			-- load gitsigns only when a git file is opened -- from https://github.com/NvChad/NvChad/blob/v2.0/lua/plugins/init.lua#L95
+			vim.api.nvim_create_autocmd({ "BufRead" }, {
+				group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+				callback = function()
+					vim.fn.system("git -C " .. vim.fn.expand("%:p:h") .. " rev-parse")
+					if vim.v.shell_error == 0 then
+						vim.api.nvim_del_augroup_by_name("GitSignsLazyLoad")
+						vim.schedule(function()
+							require("lazy").load({ plugins = { "gitsigns.nvim" } })
+						end)
+					end
+				end,
+			})
+		end,
+		config = function()
+			require("plugins.gitsigns").setup()
+		end,
+	},
 	{ "APZelos/blamer.nvim" },
 
 	-- -- Themes
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,24 +2,12 @@ local M = {}
 
 M.config = {
 	signs = {
-		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-		delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-		changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		add = { text = "▎" },
+		change = { text = "▎" },
+		delete = { text = "契" },
+		topdelete = { text = "契" },
+		changedelete = { text = "▎" },
 	},
-	numhl = false,
-	linehl = false,
-	keymaps = {
-		-- Default keymap options
-		noremap = true,
-		buffer = true,
-	},
-	max_file_length = 2000,
-	watch_gitdir = { interval = 1000 },
-	sign_priority = 6,
-	update_debounce = 500,
-	status_formatter = nil, -- Use default
 }
 
 M.setup = function()
