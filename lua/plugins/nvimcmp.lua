24039f3554cad7c529c2c973ef03be93c6f22024
Miles Crighton <miles.crighton@gmail.com>
2024-05-12 18:24:43 +0100

    Fix tab handling

--- lua/plugins/nvimcmp.lua
+++ lua/plugins/nvimcmp.lua
@@ -57,11 +57,6 @@ M.config = function()
 			["<Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_next_item()
-				elseif require("luasnip").expand_or_jumpable() then
-					vim.fn.feedkeys(
-						vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-						""
-					)
 				else
 					fallback()
 				end
@@ -72,8 +67,6 @@ M.config = function()
 			["<S-Tab>"] = cmp.mapping(function(fallback)
 				if cmp.visible() then
 					cmp.select_prev_item()
-				elseif require("luasnip").jumpable(-1) then
-					vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
 				else
 					fallback()
 				end
