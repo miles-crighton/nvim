bead1e1a30ada69dbb4e5417fe86c2f5d1597851
Miles Crighton <miles.crighton@gmail.com>
2025-01-22 16:11:10 +0000

    Use generic formatting autocommand

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,22 +1,22 @@
 local function define_augroups(definitions)
-	for group_name, definition in pairs(definitions) do
-		vim.api.nvim_create_augroup(group_name, { clear = true })
-		for _, def in pairs(definition) do
-			vim.api.nvim_create_autocmd(def[1], { group = group_name, pattern = def[2], command = def[3] })
-		end
-	end
+  for group_name, definition in pairs(definitions) do
+    vim.api.nvim_create_augroup(group_name, { clear = true })
+    for _, def in pairs(definition) do
+      vim.api.nvim_create_autocmd(def[1], { group = group_name, pattern = def[2], command = def[3] })
+    end
+  end
 end
 
 define_augroups({
-	_general_settings = {
-		{ "TextYankPost", "*", "lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})" },
-		{ "VimLeavePre", "*", "set title set titleold=" },
-		{ "CursorHold", "*", "lua vim.diagnostic.open_float()" },
-		{ "BufWritePre", "*.rs", "lua vim.lsp.buf.formatting_sync()" },
-	},
-	_markdown = { { "FileType", "markdown", "setlocal wrap" }, { "FileType", "markdown", "setlocal spell" } },
-	_buffer_bindings = {
-		{ "FileType", "lspinfo", "nnoremap <silent> <buffer> q :q<CR>" },
-		{ "FileType", "floaterm", "nnoremap <silent> <buffer> q :q<CR>" },
-	},
+  _general_settings = {
+    { "TextYankPost", "*", "lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})" },
+    { "VimLeavePre",  "*", "set title set titleold=" },
+    { "CursorHold",   "*", "lua vim.diagnostic.open_float()" },
+    { "BufWritePre",  "*", "lua vim.lsp.buf.format()" },
+  },
+  _markdown = { { "FileType", "markdown", "setlocal wrap" }, { "FileType", "markdown", "setlocal spell" } },
+  _buffer_bindings = {
+    { "FileType", "lspinfo",  "nnoremap <silent> <buffer> q :q<CR>" },
+    { "FileType", "floaterm", "nnoremap <silent> <buffer> q :q<CR>" },
+  },
 })
